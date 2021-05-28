805c2bea1269e15fcb66fd109e95c55bda3015b1
marine-jacquens <62135610+marine-jacquens@users.noreply.github.com>
2021-05-29 00:29:51 +0200

    update
    ajout exo 2 séparation JS
    ajout boutique

---
+++ TransformerFichierHtmlEnAppReact/exo2/app.js
@@ -0,0 +1,5 @@
+function MyComponent() {
+    return <div>Hello OpenClassrooms 👋</div>
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById("react-goes-here"))
---
+++ boutique/app.js
@@ -0,0 +1,5 @@
+function MyComponent() {
+    return <h1>La maison jungle</h1>
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById("root"))
